dbe356ebb32a41550e8444f03c93f991e2f02675
barbare2013 <rtveliashvili.babi13@gmail.com>
2025-07-08 16:54:12 +0400

    commit

---
+++ day 01/classwork/lesson2.py
@@ -0,0 +1,8 @@
+
+color="pink"
+color1="blue"
+color2="green"
+
+print(color)
+print(color1)
+print(color2)
---
+++ day 01/homework/homework.py
@@ -0,0 +1,17 @@
+# 1)
+color="pink"
+country="georgia"
+subject="mathematics"
+
+print(color)
+print(country)
+print(subject)
+# 2)
+
+brother="giorgi"
+father="tornike"
+mother="tatia"
+
+print(brother)
+print(father)
+print(mother)
