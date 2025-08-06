65302e4d8499be25232f4cf3b471c92b6140d656
barbare2013 <rtveliashvili.babi13@gmail.com>
2025-08-06 16:46:05 +0400

    commit

---
+++ day 5,5/homework/homework2.py
@@ -0,0 +1,29 @@
+# პრაქტიკა:
+# 1)შეასწორეთ კოდში შეცდომები:
+name = input("enter your name")
+print("hello" + name)
+
+age =  int(input("enter your age"))
+print(age+10)
+
+
+# 2)შექმენით პროგრამა, რომელიც მომხარებელს შეეკითხება სახელს, და შემდეგ მიესალმება მას
+# მაგ: 
+# input: "Aleksandre"
+# output: "Hello Aleksandre"
+name = input("enter your name")
+print("hello" + name)
+
+
+# 3)შექმენით პროგრამა რომელიც გამოითვლის რამდენი წლის იქნება მომხარებელი 10 წელში
+# მაგ:
+# input:21
+# output:31
+age = int(input("enter your age"))
+print(age+ 10)
+
+# 4)მოიძიეთ ინფორმაცია output() ფუნქციაზე, გამაგებინეთ მერე თუ იპოვით :DDDD
+# In Python, the primary function used for producing output to the console is the print() function.
+# Basic Usage:
+# The print() function can display strings, variables, and expressions.
+# პიტონში აუთფუთის მაგალითია print ფუნქცია.
