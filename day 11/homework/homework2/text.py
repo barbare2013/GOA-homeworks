4e7a59ce4cd4f281c1502ad457dcf6343a192d16
barbare2013 <rtveliashvili.babi13@gmail.com>
2025-09-15 11:14:11 +0400

    commit

---
+++ day 11/homework/homework2/text.py
@@ -0,0 +1,5 @@
+# number =int(input('enter your number'))
+# print(number > 16) 
+# # number = 18
+
+# pseudocode - Display input 'Enter your name'. Compare 16.
---
+++ day 11/homework/homework3.py
@@ -0,0 +1,6 @@
+# 3)შექმენით თქვენთვის სასურველი პროგრამა, და კომენტაარებით მიუწერეთ თითოეულ ხაზზ pseudocode კომენტარებით
+
+age = int(input('enter your age'))
+# display input enter your age
+print(age + 1999)
+# age add to it 1999
