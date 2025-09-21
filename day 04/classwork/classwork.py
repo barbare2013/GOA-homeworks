ff03b65e2ec17fe186eb60c8655396e24ba4a69b
barbare2013 <rtveliashvili.babi13@gmail.com>
2025-09-21 18:01:44 +0400

    commit

--- day 04/classwork/classwork.py
+++ day 04/classwork/classwork.py
@@ -4,8 +4,8 @@ age = 15
 city = "Paris"
 hobby = "painting"
 
-print("Hello  my name is")
-print("I am ears old")
-print("I live in city")
-print("My favorite hobby is  hobby")
+print("Hello  my name is" + name)
+print("I am" + age + "ears old")
+print("I live in city" + city )
+print("My favorite hobby is" + hobby )
 print("Nice to meet you")
