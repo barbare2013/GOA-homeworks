8588d144a19607a66d68e3619034e3406255fafa
barbare2013 <rtveliashvili.babi13@gmail.com>
2025-11-15 18:07:24 +0400

    commit

---
+++ day 18/homework/homework1.py
@@ -0,0 +1,7 @@
+
+# 3)შექმენით infinity loop ის მაგალითი
+
+i=100
+while i>1:
+    print(i)
+    i+=1
---
+++ day 18/homework/homework3.py
@@ -0,0 +1,10 @@
+# 5)გააკეთეთ password input ის პროგრამა
+
+user_pasword= input("enter your pasword: ")
+
+pasword="2013"
+
+while user_pasword != pasword:
+    print("wrong! try again")
+    user_pasword= input("enter your pasword: ")
+print("succes!")
