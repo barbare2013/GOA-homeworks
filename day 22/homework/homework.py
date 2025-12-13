5e123a74b44b2eea7ff72dac2490756fdd303539
barbare2013 <rtveliashvili.babi13@gmail.com>
2025-12-13 16:37:41 +0400

    commit

---
+++ day 22/homework/homework.py
@@ -0,0 +1,16 @@
+
+# 3)მომხარებელს შემოატანინეთ რიცხვი, დაა შემდეგ თუ მეტია ნაკლებია 18 ზე, დაპრინტეთ "kid" სხვა
+#  შემთხვევაში თუ  უდრის 15 ს დაპრინტეთ "15" სხვა შემთხვევაში დაპრინტეთ "zangebi"
+
+number=int(input('enter your number: '))
+
+if number > 18:
+    print('kid')
+elif number == 15:
+     print('15')
+else:
+     print('zangebi')
+
+
+
+
